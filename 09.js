d859e3987316d42aeae0463607502494a1dcf3a9
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2021-12-09 08:16:59 +0100

    factorize

--- 09.js
+++ 09.js
@@ -12,11 +12,12 @@ const realGrid = buildGrid("09.txt");
 const testGrid = buildGrid("09.test.txt");
 console.log();
 
-function partA(grid) {
+
+function getLowestPoint(grid) {
   let y = 0;
   let x = 0;
 
-  let risk = 0;
+  const points = [];
 
   while (grid[y][x] !== undefined) {
     const current = grid[y][x];
@@ -32,7 +33,7 @@ function partA(grid) {
 
     if (isLowest) {
       // console.log(current);
-      risk += current + 1;
+      points.push({ x, y });
     }
 
     // move cursor
@@ -42,10 +43,19 @@ function partA(grid) {
       y++;
       x = 0;
     } else {
-      return risk;
+      return points;
     }
   }
-  return risk;
+  return points;
+}
+
+function partA(grid) {
+  let risk = 0;
+
+  for (const { x, y } of getLowestPoint(grid)) {
+    risk += grid[y][x] + 1;
+  }
+  return risk
 }
 assert.strictEqual(partA(testGrid), 15);
 
@@ -53,42 +63,42 @@ console.log("Part A", partA(realGrid));
 
 ///
 
-function partB(grid) {
-  let y = 0;
-  let x = 0;
+// function partB(grid) {
+//   let y = 0;
+//   let x = 0;
 
-  let risk = 0;
+//   let risk = 0;
 
-  while (grid[y][x] !== undefined) {
-    const current = grid[y][x];
+//   while (grid[y][x] !== undefined) {
+//     const current = grid[y][x];
 
-    // console.log(`Scan ${y}*${x}`);
+//     // console.log(`Scan ${y}*${x}`);
 
-    const top = y > 0 ? grid[y - 1][x] : undefined;
-    const bottom = y < grid.length - 1 ? grid[y + 1][x] : undefined;
-    const left = x > 0 ? grid[y][x - 1] : undefined;
-    const right = x < grid[y].length - 1 ? grid[y][x + 1] : undefined;
+//     const top = y > 0 ? grid[y - 1][x] : undefined;
+//     const bottom = y < grid.length - 1 ? grid[y + 1][x] : undefined;
+//     const left = x > 0 ? grid[y][x - 1] : undefined;
+//     const right = x < grid[y].length - 1 ? grid[y][x + 1] : undefined;
 
-    const isLowest = [top, bottom, left, right].filter((c) => c !== undefined).every((c) => current < c);
+//     const isLowest = [top, bottom, left, right].filter((c) => c !== undefined).every((c) => current < c);
 
-    if (isLowest) {
-      // console.log(current);
-      risk += current + 1;
-    }
+//     if (isLowest) {
+//       // console.log(current);
+//       risk += current + 1;
+//     }
 
-    // move cursor
-    if (grid[y][x + 1] !== undefined) {
-      x++;
-    } else if (grid[y + 1] && grid[y + 1][0] !== undefined) {
-      y++;
-      x = 0;
-    } else {
-      return risk;
-    }
-  }
-  return risk;
-}
-assert.strictEqual(partB(testGrid), 15);
-console.log("Part B", partB(realGrid));
+//     // move cursor
+//     if (grid[y][x + 1] !== undefined) {
+//       x++;
+//     } else if (grid[y + 1] && grid[y + 1][0] !== undefined) {
+//       y++;
+//       x = 0;
+//     } else {
+//       return risk;
+//     }
+//   }
+//   return risk;
+// }
+// assert.strictEqual(partB(testGrid), 15);
+// console.log("Part B", partB(realGrid));
 
 // console.log(grid);
