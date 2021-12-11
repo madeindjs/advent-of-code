c6ef8642205117105ce2d027f1b2576979a26876
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2021-12-11 14:26:44 +0100

    wip

--- 10.js
+++ 10.js
@@ -61,3 +61,111 @@ assert.strictEqual(partA("10.test.txt"), 26397);
 console.log("Part A", partA("10.txt"));
 
 // console.log();
+
+// ----
+
+/**
+ * @param {string} line
+ */
+function compleClosingChar(line) {
+  let position = 0;
+
+  const stack = [];
+
+  do {
+    const char = line[position];
+
+    const openingCouple = couples.find(({ start }) => start === char);
+
+    if (openingCouple) {
+      stack.push(openingCouple);
+      position++;
+      continue;
+    }
+
+    // console.log(stack)
+
+    const lastCouple = stack.pop();
+
+    if (char !== lastCouple.end) {
+      // console.log(char);
+      // return lastCouple.p;
+      return lastCouple.end.concat(
+        stack
+          .reverse()
+          .map(({ end }) => end)
+          .join("")
+      );
+    } else {
+      position++;
+    }
+  } while (line.length > position);
+
+  if (stack.length > 0) {
+    return stack
+      .reverse()
+      .map(({ end }) => end)
+      .join("");
+  }
+
+  return "";
+}
+
+assert.strictEqual(compleClosingChar("[({(<(())[]>[[{[]{<()<>>"), "}}]])})]");
+assert.strictEqual(compleClosingChar("[(()[<>])]({[<{<<[]>>("), ")}>]})");
+assert.strictEqual(compleClosingChar("(((({<>}<{<{<>}{[]{[]{}"), "}}>}>))))");
+assert.strictEqual(compleClosingChar("{<[[]]>}<{[{[{[]{()[[[]"), "]]}}]}]}>");
+// assert.strictEqual(compleClosingChar("[(()[<>])]({[<{<<[]>>("), ")}>]})");
+
+/**
+ *
+ * @param {string} line
+ */
+function getScore(line) {
+  let score = 0n;
+
+  for (const char of line) {
+    score = score * 5n;
+
+    switch (char) {
+      case ")":
+        score += 1n;
+        break;
+      case "]":
+        score += 2n;
+        break;
+      case "}":
+        score += 3n;
+        break;
+      case ">":
+        score += 4n;
+        break;
+    }
+  }
+
+  return score;
+}
+
+assert.strictEqual(getScore("}}]])})]"), 288957n);
+assert.strictEqual(getScore(")}>]})"), 5566n);
+
+function partB(file) {
+  const scores = getLines(file).map(compleClosingChar).map(getScore);
+  console.log(scores.sort((a, b) => Number(a - b))[Math.round((scores.length + 1) / 2)]);
+
+  while (scores.length > 0) {
+    console.log(scores)
+    scores.pop()
+    scores.shift()
+  }
+
+  console.log(scores.length)
+
+}
+
+// console.log(getLines("10.test.txt").map(findClosingChar));
+// assert.strictEqual(partA("10.test.txt"), 26397);
+console.log("Part B", partB("10.txt"));
+// not 1216349671
+// not 1542093438
+// not 1186608909
