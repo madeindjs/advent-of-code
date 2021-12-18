9d5f0d0e81aa72ef72e20e8c066ba260bead78e3
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2021-12-18 19:57:05 +0100

    15b (slow)

--- 15.js
+++ 15.js
@@ -1,6 +1,8 @@
 // @ts-check
 const { readFileSync } = require("fs");
 const { strictEqual } = require("assert");
+const { off } = require("process");
+const { assert } = require("console");
 
 const getGrid = (file) =>
   readFileSync(file)
@@ -36,7 +38,8 @@ function getNeighbors(grid, point) {
  * @param {string} to
  */
 function dijkstra(grid, from, to) {
-  let unvisited = Object.keys(grid);
+  const unvisited = new Set(Object.keys(grid));
+  console.time("dijkstra");
 
   /** @type {Table} */
   const table = {
@@ -45,18 +48,19 @@ function dijkstra(grid, from, to) {
 
   const getNext = () =>
     Object.keys(table)
-      .filter((point) => unvisited.includes(point))
+      .filter((point) => unvisited.has(point))
       .sort((a, b) => table[b].distance - table[a].distance)
       .pop();
 
-  while (unvisited.length) {
+  while (unvisited.size) {
     const current = getNext();
+    console.timeLog("dijkstra", `rest ${unvisited.size}`);
     const { distance } = table[current];
 
-    const neighbors = getNeighbors(grid, current).filter((p) => unvisited.includes(p));
+    const neighbors = getNeighbors(grid, current).filter((p) => unvisited.has(p));
 
     if (neighbors.length === 0) {
-      unvisited = unvisited.filter((p) => p !== current);
+      unvisited.delete(current);
       continue;
     }
 
@@ -73,8 +77,10 @@ function dijkstra(grid, from, to) {
       }
     }
 
-    unvisited = unvisited.filter((p) => p !== current);
+    unvisited.delete(current);
   }
+
+  console.timeEnd("dijkstra");
 
   return table[to]?.distance;
 }
@@ -86,4 +92,73 @@ function partA(file) {
 }
 
 strictEqual(partA("15.test.txt"), 40);
-console.log("PartA", partA("15.txt"));
+// console.log("PartA", partA("15.txt"));
+
+/**
+ *
+ * @param {number} value
+ * @param {number} offset
+ */
+function computeValueOffset(value, offset) {
+  let newValue = value + offset;
+
+  while (newValue > 9) {
+    newValue -= 9;
+  }
+
+  return newValue;
+}
+
+strictEqual(computeValueOffset(9, 1), 1);
+strictEqual(computeValueOffset(9, 2), 2);
+strictEqual(computeValueOffset(1, 9), 1);
+
+/**
+ *
+ * @param {Grid} grid
+ * @returns {Grid}
+ */
+function increaseGrid(grid) {
+  const [maxX, maxY] = Object.keys(grid).pop().split(",").map(Number);
+
+  /** @type {Grid} */
+  const newGrid = {};
+
+  for (let xOffset = 0; xOffset < 5; xOffset++) {
+    for (let yOffset = 0; yOffset < 5; yOffset++) {
+      for (const point of Object.keys(grid)) {
+        const value = grid[point];
+        const newValue = computeValueOffset(value, xOffset + yOffset);
+        const [x, y] = point.split(",").map(Number);
+        const newPoint = `${x + xOffset * (maxX + 1)},${y + yOffset * (maxY + 1)}`;
+
+        newGrid[newPoint] = newValue;
+      }
+    }
+  }
+
+  return newGrid;
+}
+
+function displayGrid(grid) {
+  const [maxX, maxY] = Object.keys(grid).pop().split(",").map(Number);
+
+  for (let y = 0; y <= maxY; y++) {
+    let str = "";
+    for (let x = 0; x <= maxX; x++) {
+      str += grid[`${x},${y}`];
+    }
+
+    console.log(str);
+  }
+}
+
+function partB(file) {
+  const grid = increaseGrid(getGrid(file));
+  // displayGrid(grid);
+
+  return dijkstra(grid, Object.keys(grid).shift(), Object.keys(grid).pop());
+}
+
+strictEqual(partB("15.test.txt"), 315);
+console.log("PartB", partB("15.txt"));
