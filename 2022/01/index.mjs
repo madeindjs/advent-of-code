f5b85d327e4510285b299677efb9ea1ded5a295f
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2022-12-01 09:41:42 +0100

    2022-12-01

---
+++ 2022/01/index.mjs
@@ -0,0 +1,80 @@
+import { readFile } from "fs/promises";
+import assert from "node:assert";
+
+/**
+ * @param {string[]} lines
+ * @return {number[]}
+ */
+function getGuys(lines) {
+  /** @type {number[]} */
+  const guys = [];
+  let i = 0;
+
+  for (const line of lines) {
+    if (line === "") {
+      i++;
+      continue;
+    }
+
+    guys[i] ??= 0;
+    guys[i] += Number(line);
+  }
+
+  return guys;
+}
+
+/**
+ * @param {string} file
+ * @returns {Promise<number>}
+ */
+async function mainA(file) {
+  const lines = await readFile(file).then((buff) => buff.toString("utf-8").split("\n"));
+  const guys = getGuys(lines);
+
+  return Math.max(...guys);
+}
+
+/**
+ * @param {string} file
+ * @returns {Promise<number>}
+ */
+async function mainB(file) {
+  const lines = await readFile(file).then((buff) => buff.toString("utf-8").split("\n"));
+  const guys = getGuys(lines);
+
+  return getTop3Max(guys);
+}
+
+/**
+ * @param {number[]} results
+ * @returns {number}
+ */
+function getTop3Max(results) {
+  const resultsCopy = [...results];
+
+  const getMaxAndRemove = () => {
+    const max = Math.max(...resultsCopy);
+    const i = resultsCopy.findIndex((r) => r === max);
+    resultsCopy[i] = 0;
+
+    return max;
+  };
+
+  return [getMaxAndRemove(), getMaxAndRemove(), getMaxAndRemove()].reduce((a, b) => a + b, 0);
+}
+
+async function main() {
+  const testA = await mainA("spec.txt");
+  assert.strictEqual(testA, 24000);
+
+  const resultA = await mainA("input.txt");
+  console.log("result A", resultA);
+
+  const testB = await mainB("spec.txt");
+  assert.strictEqual(testB, 45000);
+
+  const resultB = await mainB("input.txt");
+  console.log("result B", resultB);
+}
+
+main().catch(console.error);
