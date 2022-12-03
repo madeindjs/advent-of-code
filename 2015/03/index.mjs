97edced613f9b1cf5880cb18bed202bab34a10ff
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2022-12-03 10:45:55 +0100

    2015-03b

--- 2015/03/index.mjs
+++ 2015/03/index.mjs
@@ -3,27 +3,44 @@ import assert from "node:assert";
 
 const content = readFileSync("input.txt").toString("utf-8");
 
+/**
+ * @param {[number, number]} position
+ * @returns {string}
+ */
+function positionToStr(position) {
+  return `${position[0]}x${position[1]}`;
+}
+
+/**
+ * @param {[number, number]} position
+ * @param {string} direction
+ */
+function updatePosition(position, direction) {
+  switch (direction) {
+    case ">":
+      position[1]++;
+      break;
+    case "v":
+      position[0]--;
+      break;
+    case "<":
+      position[1]--;
+      break;
+    case "^":
+      position[0]++;
+      break;
+  }
+}
+
 function getHomes(path) {
+  /** @type {[number, number]} */
   let current = [0, 0];
   const points = new Set();
-  points.add(`${current[0]}x${current[1]}`);
+  points.add(positionToStr(current));
 
-  for (const position of path.split("")) {
-    switch (position) {
-      case ">":
-        current[1]++;
-        break;
-      case "v":
-        current[0]--;
-        break;
-      case "<":
-        current[1]--;
-        break;
-      case "^":
-        current[0]++;
-        break;
-    }
-    points.add(`${current[0]}x${current[1]}`);
+  for (const direction of path.split("")) {
+    updatePosition(current, direction);
+    points.add(positionToStr(current));
   }
   // console.log(points.values());
   return points.size;
@@ -33,3 +50,27 @@ assert.strictEqual(getHomes("^>v<"), 4);
 assert.strictEqual(getHomes("^v^v^v^v^v"), 2);
 
 console.log("Part A", getHomes(content));
+
+function getHomesWithRobot(path) {
+  /** @type {[number, number]} */
+  let currentSanta = [0, 0];
+  /** @type {[number, number]} */
+  let currentRobot = [0, 0];
+  const points = new Set();
+  points.add(positionToStr(currentSanta));
+
+  let isSanta = true;
+
+  for (const direction of path.split("")) {
+    updatePosition(isSanta ? currentSanta : currentRobot, direction);
+    points.add(positionToStr(isSanta ? currentSanta : currentRobot));
+    isSanta = !isSanta;
+  }
+  return points.size;
+}
+assert.strictEqual(getHomesWithRobot(">"), 2);
+assert.strictEqual(getHomesWithRobot("^>v<"), 3);
+assert.strictEqual(getHomesWithRobot("^v^v^v^v^v"), 11);
+
+console.log("Part B", getHomesWithRobot(content));
+// 2837
