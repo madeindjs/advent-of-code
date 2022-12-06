7f9d4beabcda9733ab9ef519171e98de0857224c
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2022-12-06 09:06:59 +0100

    2022-06b

--- 2022/06/index.mjs
+++ 2022/06/index.mjs
@@ -2,29 +2,35 @@ import { readFileSync } from "fs";
 import assert from "node:assert";
 
 /**
- *
  * @param {string} string
+ * @param {number} size
  * @returns {number}
  */
-function findMarker(string) {
+function findMarker(string, size) {
   let cursor = 0;
 
-  while (string[cursor + 4]) {
-    const letters = string.split("").slice(cursor, cursor + 4);
+  while (string[cursor + size]) {
+    const letters = string.split("").slice(cursor, cursor + size);
 
     if (letters.every((letter) => letters.filter((l) => l === letter).length === 1)) {
-      return cursor + 4;
+      return cursor + size;
     }
     cursor++;
   }
 
   return -1;
 }
-assert.strictEqual(findMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-assert.strictEqual(findMarker("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-assert.strictEqual(findMarker("nppdvjthqldpwncqszvftbrmjlhg"), 6);
-assert.strictEqual(findMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-assert.strictEqual(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+assert.strictEqual(findMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4), 7);
+assert.strictEqual(findMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), 5);
+assert.strictEqual(findMarker("nppdvjthqldpwncqszvftbrmjlhg", 4), 6);
+assert.strictEqual(findMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4), 10);
+assert.strictEqual(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4), 11);
+
+assert.strictEqual(findMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14), 19);
+assert.strictEqual(findMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14), 23);
+assert.strictEqual(findMarker("nppdvjthqldpwncqszvftbrmjlhg", 14), 23);
+assert.strictEqual(findMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14), 29);
+assert.strictEqual(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14), 26);
 
 /**
  * @param {string} file
@@ -32,35 +38,21 @@ assert.strictEqual(findMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
  */
 function mainA(file) {
   const content = readFileSync(file).toString("utf-8");
-  return findMarker(content);
+  return findMarker(content, 4);
 }
 
-// /**
-//  * @param {string} file
-//  * @returns {string}
-//  */
-// function mainB(file) {
-//   const content = readFileSync(file).toString("utf-8");
-
-//   const [stacksScheme, moves] = content.split("\n\n");
-//   const stacks = parseStacks(stacksScheme);
-
-//   for (const line of moves.split("\n")) {
-//     const { from, to, qty } = parseMove(line);
-
-//     const items = stacks[from].splice(stacks[from].length - qty);
-//     stacks[to].push(...items);
-//   }
-
-//   return stacks.map((stack) => stack[stack.length - 1]).join("");
-// }
+/**
+ * @param {string} file
+ * @returns {number}
+ */
+function mainB(file) {
+  const content = readFileSync(file).toString("utf-8");
+  return findMarker(content, 14);
+}
 
 function main() {
-  // assert.strictEqual(mainA("spec.txt"), "CMZ");
   console.log("result A", mainA("input.txt"));
-
-  // assert.strictEqual(mainB("spec.txt"), "MCD");
-  // console.log("result B", mainB("input.txt"));
+  console.log("result B", mainB("input.txt"));
 }
 
 main();
