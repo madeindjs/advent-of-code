1ee06bca977f29c2f2084feccf20d2868a98a4ea
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-05 12:31:31 +0100

    add progress bar

--- 2023/05/index.mjs
+++ 2023/05/index.mjs
@@ -16,7 +16,7 @@ function isInRange(nb, start, range) {
 assert.strictEqual(isInRange(51, 50, 2), true);
 
 function applyTransform(nb, from, to, range) {
-  if (!isInRange(nb, from, range)) return nb;
+  // if (!isInRange(nb, from, range)) return nb;
   const offset = to - from;
   return nb + offset;
 }
@@ -74,23 +74,30 @@ assert.deepEqual(Array.from(createRanges([0, 2, 6, 2])), [0, 1, 6, 7]);
 
 function mainB(file) {
   const linesGroups = readFileSync(file, { encoding: "utf-8" }).split("\n\n");
-  const seedsGenerator = createRanges(extractNubersFromStr(linesGroups.shift()));
+  const seedsNbs = extractNubersFromStr(linesGroups.shift());
+  const total = seedsNbs.filter((_, i) => i % 2 === 1).reduce((acc, v) => acc + v, 0);
+  const seedsGenerator = createRanges(seedsNbs);
   const instructions = linesGroups.map(parseInstruction);
 
   let min = Infinity;
 
+  let i = 0;
+
   for (const start of seedsGenerator) {
+    i++;
     let type = "seed";
     let qty = start;
 
+    if (i % 1000000 === 0) {
+      console.log(`Progress ${Math.round((i / total) * 100)}% (${Math.round(i / 100)}K/${Math.round(total / 100)}K)`);
+    }
+
     for (const { from, to, trans } of instructions) {
       if (from !== type) continue;
-
       type = to;
       qty = applyTransforms(qty, trans);
-
-      if (qty < min && type === "location") min = qty;
     }
+    if (qty < min && type === "location") min = qty;
   }
 
   return min;
