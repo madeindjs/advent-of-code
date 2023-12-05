1af71a2e7bf8f5a27792389830fbc8a4af2f4932
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-05 14:15:33 +0100

    cleaning

--- 2023/05/index.mjs
+++ 2023/05/index.mjs
@@ -15,21 +15,16 @@ function isInRange(nb, start, range) {
 }
 assert.strictEqual(isInRange(51, 50, 2), true);
 
-function applyTransform(nb, from, to, range) {
-  // if (!isInRange(nb, from, range)) return nb;
-  const offset = to - from;
-  return nb + offset;
-}
 /**
  * @param {number} nb
  * @param {number[][]} trans
- * @returns
+ * @returns {number}
  */
 function applyTransforms(nb, trans) {
   for (const [to, from, range] of trans) {
-    if (isInRange(nb, from, range)) {
-      return applyTransform(nb, from, to, range);
-    }
+    if (!isInRange(nb, from, range)) continue;
+    const offset = to - from;
+    return nb + offset;
   }
   return nb;
 }
@@ -44,15 +39,14 @@ function parseInstruction(lineGroup) {
 
 function mainA(file) {
   const linesGroups = readFileSync(file, { encoding: "utf-8" }).split("\n\n");
-
   const map = { seed: extractNubersFromStr(linesGroups.shift()) };
+  const instructions = linesGroups.map(parseInstruction);
 
-  for (const lineGroup of linesGroups) {
-    const { from, to, trans } = parseInstruction(lineGroup);
-
+  for (const { from, to, trans } of instructions) {
     map[to] = (map[from] ?? []).map((s) => applyTransforms(s, trans));
     map[from] = [];
   }
+
   return Math.min(...Object.values(map).flatMap((m) => m));
 }
 
@@ -75,22 +69,16 @@ assert.deepEqual(Array.from(createRanges([0, 2, 6, 2])), [0, 1, 6, 7]);
 function mainB(file) {
   const linesGroups = readFileSync(file, { encoding: "utf-8" }).split("\n\n");
   const seedsNbs = extractNubersFromStr(linesGroups.shift());
-  const total = seedsNbs.filter((_, i) => i % 2 === 1).reduce((acc, v) => acc + v, 0);
-  const seedsGenerator = createRanges(seedsNbs);
   const instructions = linesGroups.map(parseInstruction);
 
   let min = Infinity;
 
   let i = 0;
 
-  for (const start of seedsGenerator) {
+  for (const start of createRanges(seedsNbs)) {
     i++;
     let type = "seed";
     let qty = start;
-
-    if (i % 1000000 === 0) {
-      console.log(`Progress ${Math.round((i / total) * 100)}% (${Math.round(i / 100)}K/${Math.round(total / 100)}K)`);
-    }
 
     for (const { from, to, trans } of instructions) {
       if (from !== type) continue;
