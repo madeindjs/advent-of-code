bc8fdce150c60b933d1d52f0f3d344b9bca351b5
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-06 10:46:17 +0100

    2023-06b

--- 2023/06/index.mjs
+++ 2023/06/index.mjs
@@ -1,8 +1,9 @@
 import assert from "node:assert";
 import { readFileSync } from "node:fs";
 
+const extractNumbers = (str) => Array.from(str.matchAll(/ ?([0-9]+)/g)).map((match) => Number(match[1]));
+
 function* parse(file) {
-  const extractNumbers = (str) => Array.from(str.matchAll(/ ?([0-9]+)/g)).map((match) => Number(match[1]));
   const [times, distances] = readFileSync(file, { encoding: "utf-8" }).split("\n").map(extractNumbers);
 
   for (let i = 0; i < times.length; i++) {
@@ -15,6 +16,14 @@ assert.deepEqual(Array.from(parse("./spec.txt")), [
   [30, 200],
 ]);
 
+function parse2(file) {
+  return readFileSync(file, { encoding: "utf-8" })
+    .split("\n")
+    .map(extractNumbers)
+    .map((a) => Number(a.join("")));
+}
+assert.deepEqual(parse2("./spec.txt"), [71530, 940200]);
+
 /**
  * @param {number} distance
  * @param {number} holdMs
@@ -26,12 +35,10 @@ assert.strictEqual(computeTime(1, 10), 11);
 assert.strictEqual(computeTime(5, 10), 7);
 assert.strictEqual(computeTime(4, 10), 6.5);
 
-function* getBetterSolutions(timeToBeat, distance) {
-  let possibilities = new Array(timeToBeat).fill(0).map((_, i) => i);
-
-  for (const holdMs of possibilities) {
-    const timeSpent = computeTime(holdMs, distance);
-    if (timeSpent < timeToBeat) yield holdMs;
+function* getBetterSolutions(timeToBeat, distance, from = 1) {
+  for (let i = from; i < timeToBeat; i++) {
+    const timeSpent = computeTime(i, distance);
+    if (timeSpent < timeToBeat) yield i;
   }
 }
 assert.deepEqual(Array.from(getBetterSolutions(7, 9)), [2, 3, 4, 5]);
@@ -46,3 +53,10 @@ function mainA(file) {
 }
 assert.strictEqual(mainA("./spec.txt"), 288);
 console.log(mainA("./input.txt"));
+
+function mainB(file) {
+  const [timeToBeat, distance] = parse2(file);
+  return Array.from(getBetterSolutions(timeToBeat, distance)).length;
+}
+assert.strictEqual(mainB("./spec.txt"), 71503);
+console.log(mainB("./input.txt"));
