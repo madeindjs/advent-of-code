f2b4b5b943dd35996cb7a0abedd863036151378b
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-07 12:22:16 +0100

    rafactor

--- 2023/07/index.mjs
+++ 2023/07/index.mjs
@@ -21,7 +21,7 @@ const getCardValue = (c) => {
 
 /**
  * @param {string} hand
- * @returns {number[]}
+ * @returns {number}
  */
 function getCardHandValue(hand) {
   /** @type {Record<number, number>} */
@@ -32,75 +32,35 @@ function getCardHandValue(hand) {
     return acc;
   }, {});
 
-  // @ts-ignore
-  const findByCount = (c) => Number(Object.entries(charCount).find(([k, v]) => v === c)[0]);
-
   const counts = Object.values(charCount);
   if (counts.includes(5)) {
-    return [COMBINAISONS.SAME5];
-    return [COMBINAISONS.SAME5, findByCount(5)];
+    return COMBINAISONS.SAME5;
   } else if (counts.includes(4)) {
-    return [COMBINAISONS.SAME4];
-    return [COMBINAISONS.SAME4, findByCount(4)];
-    // return [COMBINAISONS.SAME4, findByCount(4), findByCount(1)];
+    return COMBINAISONS.SAME4;
   } else if (counts.includes(3) && counts.includes(2)) {
-    return [COMBINAISONS.FULL];
-    return [COMBINAISONS.FULL, findByCount(3)];
-    // return [COMBINAISONS.FULL, findByCount(3), findByCount(2)];
+    return COMBINAISONS.FULL;
   } else if (counts.includes(3)) {
-    const kicker = Math.max(
-      ...Object.keys(charCount)
-        .filter(([k, v]) => Number(v) !== 3)
-        .map(([k, v]) => Number(k))
-    );
-
-    return [COMBINAISONS.SAME3];
-    return [COMBINAISONS.SAME3, findByCount(3)];
-    // return [COMBINAISONS.SAME3, findByCount(3), kicker];
+    return COMBINAISONS.SAME3;
   } else if (counts.filter((v) => v === 2).length === 2) {
-    const [a, b] = Object.entries(charCount)
-      .filter(([k, v]) => Number(v) === 2)
-      .map(([k, v]) => k)
-      .sort((a, b) => Number(a) - Number(b))
-      .map(Number);
-    return [COMBINAISONS.SAME2x2];
-    return [COMBINAISONS.SAME2x2, Math.max(a, b)];
-    return [COMBINAISONS.SAME2x2, Math.max(a, b), Math.min(a, b)];
-    // return [COMBINAISONS.SAME2x2, Math.max(a, b), Math.min(a, b), findByCount(1)];
+    return COMBINAISONS.SAME2x2;
   } else if (counts.includes(2)) {
-    const kicker = Math.max(
-      ...Object.keys(charCount)
-        .filter(([k, v]) => Number(v) !== 2)
-        .map(([k, v]) => Number(k))
-    );
-
-    return [COMBINAISONS.SAME2];
-    return [COMBINAISONS.SAME2, findByCount(2)];
-    // return [COMBINAISONS.SAME2, kicker];
+    return COMBINAISONS.SAME2;
   } else {
-    return [COMBINAISONS.NONE];
-    return [COMBINAISONS.NONE, Math.max(...Object.keys(charCount).map(Number))];
+    return COMBINAISONS.NONE;
   }
 }
-// assert.deepEqual(getCardHandValue("KKKKK"), [6, 11]);
-// assert.deepEqual(getCardHandValue("KKKK2"), [5, 11]);
-// assert.deepEqual(getCardHandValue("KKTT2"), [2, 11]);
-// assert.deepEqual(getCardHandValue("KQJT2"), [0, 11]);
-// assert.deepEqual(getCardHandValue("QQQJA"), [3, 10]);
-// assert.deepEqual(getCardHandValue("JK5QK"), [1, 11]);
+assert.strictEqual(getCardHandValue("KKKKK"), COMBINAISONS.SAME5);
+assert.strictEqual(getCardHandValue("KKKK2"), COMBINAISONS.SAME4);
+assert.strictEqual(getCardHandValue("KKTT2"), COMBINAISONS.SAME2x2);
+assert.strictEqual(getCardHandValue("KQJT2"), COMBINAISONS.NONE);
 
 /**
  * @param {Hand} a
  * @param {Hand} b
  */
 function compareHands(a, b) {
-  const max = Math.max(a.value.length, b.value.length);
-  for (let i = 0; i < max; i++) {
-    const aa = a.value[i] ?? 0;
-    const bb = b.value[i] ?? 0;
-    if (aa === bb) continue;
-    return aa > bb ? -1 : 1;
-  }
+  if (a.value !== b.value) return a.value > b.value ? -1 : 1;
+
   // samke rank, compare card
   for (let i = 0; i < a.hands.length; i++) {
     const aC = a.hands[i];
@@ -118,7 +78,7 @@ assert.strictEqual(compareHands(parseCard("23456 1"), parseCard("23457 1")), 1);
 assert.strictEqual(compareHands(parseCard("JK938 1"), parseCard("JK6T5 1")), -1);
 assert.strictEqual(compareHands(parseCard("TTTT2 1"), parseCard("TTTT3 1")), 1);
 /**
- * @typedef {{hands: string, bid: number, value: number[] }} Hand
+ * @typedef {{hands: string, bid: number, value: number }} Hand
  * @param {*} line
  * @returns {Hand}
  */
@@ -132,8 +92,7 @@ function parseCard(line) {
  */
 function mainA(file) {
   const hands = readFileSync(file).toString("utf-8").split("\n").map(parseCard).sort(compareHands).reverse();
-  console.log(hands);
-
+  // console.log(hands);
   return hands.reduce((acc, v, i) => v.bid * (i + 1) + acc, 0);
 }
 
@@ -152,13 +111,7 @@ const partA = mainA("input.txt");
 
 console.log("part A", partA);
 // too high 252663908
-assert.ok(252663908 > partA);
-assert.ok(252916108 > partA);
-assert.ok(252432368 < partA);
-assert.ok(252655003 != partA);
-assert.ok(252647010 != partA);
-assert.ok(252662531 != partA);
-assert.ok(252662752 != partA);
+assert.strictEqual(partA, 252656917);
 
 // assert.strictEqual(partA, 1770595);
 
