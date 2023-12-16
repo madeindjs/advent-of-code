346572516d09366468f296795aa0e6d700caa370
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-16 19:10:43 +0100

    reafctor

--- 2023/15/index.mjs
+++ 2023/15/index.mjs
@@ -27,3 +27,35 @@ function mainA(file) {
 }
 assert.strictEqual(mainA("spec.txt"), 1320);
 assert.strictEqual(mainA("input.txt"), 507666);
+
+function mainB(file) {
+  const boxes = new Array(256).fill("").map(() => ({}));
+  let total = 0;
+
+  for (const str of parseFile(file)) {
+    if (str.endsWith("-")) {
+      const label = str.replace("-", "");
+      const value = hash(label);
+      delete boxes[value][label];
+    } else if (str.includes("=")) {
+      const [label, slot] = str.split("=");
+      const value = hash(label);
+      boxes[value][label] = Number(slot);
+    }
+  }
+  console.log(boxes);
+
+  for (let i = 0; i < boxes.length; i++) {
+    const lens = boxes[i];
+
+    let j = 0;
+    for (const [label, value] of Object.entries(lens)) {
+      const inc = (i + 1) * (j + 1) * value;
+      total += inc;
+      j++;
+    }
+  }
+  return total;
+}
+assert.strictEqual(mainB("spec.txt"), 145);
+assert.strictEqual(mainB("input.txt"), 233537);
