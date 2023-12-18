659e91713bb4ae73778461ab972a040db9b226ce
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2023-12-18 12:52:18 +0100

    2023-17b

--- 2023/17/index.mjs
+++ 2023/17/index.mjs
@@ -4,7 +4,7 @@ import { readFileSync } from "node:fs";
 /**
  * @typedef {number[][]} Grid
  * @typedef {{x: number, y: number}} Point
- * @typedef {{x: number, y: number, direction: number, directionCount: number, distance: number, from?: Point}} Vector
+ * @typedef {{x: number, y: number, direction: number, directionCount: number, distance: number, from?: Vector}} Vector
  */
 
 /**
@@ -31,13 +31,14 @@ const colorize = (str) => `\x1b[36m${str}\x1b[0m`;
  * @param {Grid} grid
  * @param {Point} start
  * @param {Point} end
+ * @param {{max: number, min: number}} directionConstraints
  * @returns {number}
  */
-function dijkstra(grid, start, end) {
+function dijkstra(grid, start, end, directionConstraints) {
   /** @type {Vector[]} */
   const queue = [
-    { ...start, direction: DIRECTIONS.H, directionCount: 1, distance: 0, from: start },
-    { ...start, direction: DIRECTIONS.V, directionCount: 1, distance: 0, from: start },
+    { ...start, direction: DIRECTIONS.H, directionCount: 1, distance: 0 },
+    { ...start, direction: DIRECTIONS.V, directionCount: 1, distance: 0 },
   ];
 
   /** @type {Map<string, {from?: Vector, distance: number}>} */
@@ -45,7 +46,8 @@ function dijkstra(grid, start, end) {
 
   /** @param {Vector} v */
   function getVectorKey(v) {
-    return `${v.x},${v.y},${v.direction},${v.directionCount}`;
+    // return v.x << 4 | v.y << 8 | v.directionCount <<
+    return `${v.x},${v.y},${v.direction},${v.directionCount},${v.from?.x},${v.from?.y}`;
   }
 
   /** @returns {Vector} */
@@ -74,15 +76,30 @@ function dijkstra(grid, start, end) {
     console.log(clone.map((r) => r.join("")).join("\n"));
   }
 
+  /** @param {Vector} v */
+  function printSolution(v) {
+    const clone = structuredClone(grid);
+
+    let current = v.from;
+    while (current) {
+      const { x, y } = current;
+      // @ts-ignore
+      clone[x][y] = colorize(clone[x][y]);
+      // @ts-ignore
+      current = visited.get(getVectorKey(current))?.from;
+    }
+
+    console.log(clone.map((r) => r.join("")).join("\n"));
+  }
+
   while (true) {
     const vector = getNext();
     if (!vector) throw new Error();
-    // if (!vector) return best.get(`${end.x},${end.y}`) ?? Infinity;
 
     const vectorKey = getVectorKey(vector);
     if (visited.has(vectorKey)) continue;
 
-    visited.set(vectorKey, { distance: vector.distance });
+    visited.set(vectorKey, { distance: vector.distance, from: vector.from });
 
     const neighbors = MOVES.map(([tx, ty, td]) => ({
       x: vector.x + tx,
@@ -90,16 +107,23 @@ function dijkstra(grid, start, end) {
       direction: td,
       directionCount: vector.direction === td ? vector.directionCount + 1 : 1,
       distance: vector.distance + grid[vector.x + tx]?.[vector.y + ty],
-      from: { x: vector.x, y: vector.y },
+      from: vector,
     }))
-      .filter(({ directionCount }) => directionCount <= 3)
+      .filter((v) => v.direction === vector.direction || vector.directionCount >= directionConstraints.min)
+      .filter(({ directionCount }) => directionCount <= directionConstraints.max)
       .filter(({ x, y }) => !(x === vector.from?.x && y === vector.from?.y))
       .filter(({ x, y }) => grid[x]?.[y] !== undefined);
 
     for (const next of neighbors) {
-      if (end.x === next.x && end.y === next.y) return next.distance;
+      if (end.x === next.x && end.y === next.y) {
+        if (next.directionCount >= directionConstraints.min) {
+          if (process.env.DEBUG) printSolution(next);
 
-      queue.push(next);
+          return next.distance;
+        }
+      } else {
+        queue.push(next);
+      }
     }
 
     if (false) {
@@ -109,17 +133,34 @@ function dijkstra(grid, start, end) {
   }
 }
 
-function mainA(file) {
+/**
+ *
+ * @param {string} file
+ * @param {{max: number, min: number}} directionConstraints
+ * @returns
+ */
+function main(file, directionConstraints) {
   const grid = parseFile(file);
   /** @type {Point} */
   const start = { x: 0, y: 0 };
   /** @type {Point} */
   const end = { x: grid.length - 1, y: grid[0].length - 1 };
 
-  return dijkstra(grid, start, end);
+  return dijkstra(grid, start, end, directionConstraints);
 }
 
-assert.strictEqual(mainA("spec.txt"), 102);
+const mainA = (file) => main(file, { min: 0, max: 3 });
+const mainB = (file) => main(file, { min: 4, max: 10 });
+
+// assert.strictEqual(mainA("spec.txt"), 102);
+// const a = mainA("input.txt");
+// assert.strictEqual(a, 755);
+// console.log("part A", a);
+
+assert.strictEqual(mainB("spec.txt"), 94);
+assert.strictEqual(mainB("spec2.txt"), 71);
 console.log("test done");
-const a = mainA("input.txt");
-assert.strictEqual(a, 755);
+const b = mainB("input.txt");
+// assert.strictEqual(b, 755);
+console.log("part B", b);
+assert.ok(b > 879);
