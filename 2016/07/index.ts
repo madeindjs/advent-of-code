f255f1969cf09ba89232b7b530b1fcb48d8ac6ae
Alexandre Rousseau <alexandre.rousseau@contentsquare.com>
2024-11-18 16:43:35 +0100

    2016-07a

---
+++ 2016/07/index.ts
@@ -0,0 +1,60 @@
+import assert from "node:assert";
+import { createReadStream } from "node:fs";
+import readline from "node:readline";
+
+function getLines(path: string) {
+  const file = new URL(path, import.meta.url);
+  return readline.createInterface({ input: createReadStream(file) });
+}
+
+type Adress = { inside: string[]; outside: [] };
+
+function parse(line: string): Adress {
+  const adress: Adress = { inside: [], outside: [] };
+
+  let isInside = false;
+  for (const match of line.split(/\[|\]/)) {
+    (isInside ? adress.inside : adress.outside).push(match);
+    isInside = !isInside;
+  }
+
+  return adress;
+}
+assert.deepEqual(parse("abba[mnop]qrst"), {
+  inside: ["mnop"],
+  outside: ["abba", "qrst"],
+});
+assert.deepEqual(parse("abba[mnop]qrst[a]b"), {
+  inside: ["mnop", "a"],
+  outside: ["abba", "qrst", "b"],
+});
+
+function containsAbba(line: string) {
+  for (let i = 0; i < line.length - 3; i++) {
+    if (line[i] === line[i + 1]) continue;
+    if (line[i] === line[i + 3] && line[i + 1] === line[i + 2]) return true;
+  }
+  return false;
+}
+assert.strictEqual(containsAbba("abba"), true);
+assert.strictEqual(containsAbba("ioxxoj"), true);
+assert.strictEqual(containsAbba("asdfgh"), false);
+assert.strictEqual(containsAbba("abab"), false);
+
+function hasTLS(adress: Adress): boolean {
+  return !adress.inside.some(containsAbba) && adress.outside.some(containsAbba);
+}
+assert.strictEqual(hasTLS(parse("abba[mnop]qrst")), true);
+assert.strictEqual(hasTLS(parse("aaba[mnop]abba")), true);
+assert.strictEqual(hasTLS(parse("abcd[bddb]xyyx")), false);
+assert.strictEqual(hasTLS(parse("aaaa[qwer]tyui")), false);
+assert.strictEqual(hasTLS(parse("ioxxoj[asdfgh]zxcvbn")), true);
+
+async function mainA(path: string) {
+  let total = 0;
+  for await (const line of getLines(path)) {
+    if (hasTLS(parse(line))) total++;
+  }
+  return total;
+}
+assert.strictEqual(await mainA("./input.txt"), 0);
