727008f0bde3c98b55678a3f615adbd94ef89bab
Alexandre Rousseau <alexandre@rsseau.fr>
2024-12-08 13:02:52 +0100

    2024-07a

--- 2024/07/index.ts
+++ 2024/07/index.ts
@@ -10,24 +10,40 @@ function parse(line: string) {
 
 function isMatching(result: number, numbers: number[], count = 0): boolean {
   const n = numbers.at(0);
-  if (!n) return result === count;
+  if (n === undefined) return result === count;
 
-  return (
-    isMatching(result, numbers.slice(1), n + count) ||
-    isMatching(result, numbers.slice(1), n * count)
-  );
+  return [n + count, n * count]
+    .filter((next) => next <= result)
+    .some((next) => isMatching(result, numbers.slice(1), next));
 }
 
-async function mainA(path: string) {
+function isMatching2(result: number, numbers: number[], count = 0): boolean {
+  const n = numbers.at(0);
+  if (n === undefined) return result === count;
+
+  return [n + count, n * count, Number(`${count ?? ""}${n ?? "0"}`)]
+    .filter((next) => next <= result)
+    .some((next) => isMatching2(result, numbers.slice(1), next));
+}
+assert.ok(isMatching2(156, [15, 6]));
+assert.ok(isMatching2(123, [1, 2, 3]));
+
+async function main(
+  path: string,
+  isMatching: (result: number, numbers: number[], count: number) => boolean,
+) {
   const file = await open(new URL(path, import.meta.url));
 
   let total = 0;
   for await (const line of file.readLines()) {
     const { result, numbers } = parse(line);
-    if (isMatching(result, numbers)) total += result;
+    if (isMatching(result, numbers, 0)) total += result;
   }
   return total;
 }
 
-assert.strictEqual(await mainA("./spec.txt"), 3749);
-assert.strictEqual(await mainA("./input.txt"), 0);
+assert.strictEqual(await main("./spec.txt", isMatching), 3749);
+assert.strictEqual(await main("./input.txt", isMatching), 2654749936343);
+
+assert.strictEqual(await main("./spec.txt", isMatching2), 11387);
+assert.strictEqual(await main("./input.txt", isMatching2), 0);
